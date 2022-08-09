8b633933a5389dc1d3d28b62a04f4cd7ba827b13
Adefemi Adelaja <adefemiadelaja@gmail.com>
2022-08-09 14:39:16 +0100

    first commit

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { router as blogsRouter } from "./routers/blogsRouter.js";
-import { router as homePageRouter } from "./routers/homePageRouter.js";
-import { router as pageNotFoundRouter } from "./routers/pageNotFound.js";
+// import { router as homePageRouter } from "./routers/homePageRouter.js";
+// import { router as pageNotFoundRouter } from "./routers/pageNotFound.js";
 
 const PORT = 4500;
 const app = express();
@@ -12,9 +12,9 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/", homePageRouter);
-app.use("/api/blogs", blogsRouter);
-app.use("/*", pageNotFoundRouter);
+app.use("/", blogsRouter);
+// app.use("/api/blogs", blogsRouter);
+// app.use("/*", pageNotFoundRouter);
 
 app.listen(process.env.PORT || PORT, () => {
   console.log(`Server running on port ${PORT}...`);
